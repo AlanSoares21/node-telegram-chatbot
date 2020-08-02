41f3c258bd9fe599a5070727489dadd8440f6904
AlanSoares21 <alansoares26@outlook.com>
2020-08-02 20:43:48 -0300

    recebendo menssagem e respondendo o usuario

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
-// const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api');
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
 const bot = new TelegramBot(token, {polling: true});//ideal é utilizar webhooks
 
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;//id da conversa
+    console.log(msg.text);
+    bot.sendMessage(chatId, 'Received your message: '+msg.text);
+  });
